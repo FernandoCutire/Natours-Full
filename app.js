b2c91bab081288ca56d8a59af1723d274a4c9c85
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-11-24 22:45:37 -0500

    updated in controllers webhook

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const cors = require('cors');
@@ -79,7 +80,7 @@ app.use('/api', limiter);
 
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
+  bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
-
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT REJECTION!! Shutting down..');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -28,7 +27,7 @@ mongoose
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
