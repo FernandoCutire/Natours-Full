c91ce083c5f4a246c44450881fa375dfdd3caec2
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-11-25 07:13:27 -0500

    Change to line_items

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -32,7 +32,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         quantity: 1,
       },
     ],
-    amount_total: tour.price * 100,
   });
 
   // 3) Create session as response
@@ -57,7 +56,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.amount_total / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 });
 
