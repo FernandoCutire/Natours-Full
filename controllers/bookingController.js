0e57009a280f34be105d57bcfabb8a917846c04e
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-11-24 21:59:14 -0500

    Stripe booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${tour.name} Tour`,
         description: tour.summary,
         // Need to change images once deployed website
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,12 +5,9 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/',
-  // bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+router.use(viewsController.alerts);
+
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
