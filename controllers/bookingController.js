25c33af072adafcf4ae4ae61c4e0d3fc4f74cc6e
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-11-24 22:30:51 -0500

    User not defined error solved

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
-const Userr = require('../models/userModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
